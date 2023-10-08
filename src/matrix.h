38f64756847d66d542b9f5496a130f1041901cc0
Michozord <66361088+Michozord@users.noreply.github.com>
2023-10-08 21:04:40 +0200

    Impemented Matrix class with basic functionality

---
+++ src/matrix.h
@@ -0,0 +1,130 @@
+#ifndef FILE_MATRIX_H
+#define FILE_MATRIX_H
+
+#include <iostream>
+
+namespace ASC_bla
+{
+  enum ORDERING { ColMajor, RowMajor };
+  template <typename T, ORDERING ORD>
+  class Matrix
+  {
+
+    size_t rows_;
+    size_t cols_;
+    T * data_;
+    
+  public:
+    Matrix (size_t rows, size_t cols) 
+      : rows_(rows), cols_(cols), data_(new T[rows_ * cols_]) { ; }
+    
+    Matrix (const Matrix & m)
+      : Matrix(m.Rows(), m.Cols())
+    {
+      *this = m;
+    }
+
+    Matrix (Matrix && m)
+      : rows_{0}, cols_{0}, data_(nullptr)
+    {
+      std::swap(rows_, m.rows_);
+      std::swap(cols_, m.cols_);
+      std::swap(data_, m.data_);
+    }
+
+    ~Matrix () { delete [] data_; }
+    
+    Matrix & operator=(const Matrix & m2){
+        rows_ = m2.Rows();
+        cols_ = m2.Cols();
+        if (ORD == ORDERING::ColMajor){
+            for(size_t i = 0; i < m2.Rows(); i++){
+                for(size_t j = 0; j< m2.Cols(); j++){
+                    data_[j * m2.Rows() + i] = m2(i, j);
+                }
+            }
+        }else{
+            for(size_t i = 0; i < m2.Rows(); i++){
+                for(size_t j = 0; j< m2.Cols(); j++){
+                    data_[i * m2.Cols() + j] = m2(i, j);
+                }
+            }
+        }
+        return *this;
+    }
+
+    Matrix & operator= (Matrix && m2){
+        rows_ = m2.Rows();
+        cols_ = m2.Cols();
+        if (ORD == ORDERING::ColMajor){
+            for(size_t i = 0; i < m2.Rows(); i++){
+                for(size_t j = 0; j< m2.Cols(); j++){
+                    data_[j * m2.Rows() + i] = m2(i, j);
+                }
+            }
+        }else{
+            for(size_t i = 0; i < m2.Rows(); i++){
+                for(size_t j = 0; j< m2.Cols(); j++){
+                    data_[i * m2.Cols() + j] = m2(i, j);
+                }
+            }
+        }
+        return *this;
+    }
+    
+    size_t Rows() const { return rows_; }
+    size_t Cols() const { return cols_; }
+    T & operator()(size_t i, size_t j) { 
+        if (ORD == ORDERING::ColMajor){
+            return data_[j * Rows() + i];
+        } else {
+            return data_[i * Cols() + j];
+        }
+    }
+    const T & operator()(size_t i, size_t j) const {
+        if (ORD == ORDERING::ColMajor){
+            return data_[j * Rows() + i];
+        } else { 
+            return data_[i * Cols() + j];
+        } 
+    }
+
+    Matrix<T, ORD> transpose(){
+        Matrix<T, ORD> res(Cols(), Rows());
+        for (size_t i = 0; i < Rows(); i++){
+            for(size_t j = 0; j < Cols(); j++){
+                res(j, i) = (*this)(i, j);
+            }
+        }
+        return res;
+    }
+  };
+
+
+  template <typename T, ORDERING ORD>
+  std::ostream & operator<< (std::ostream & ost, const Matrix<T, ORD> & m)
+  {
+    for (size_t i = 0; i < m.Rows(); i++){
+        for(size_t j = 0; j < m.Cols() - 1; j++){
+            ost << m(i, j) << ", ";
+        }
+        ost << m(i, m.Cols() - 1) << "\n";
+    }
+    return ost;
+  }
+
+  template <typename T, ORDERING ORD1, ORDERING ORD2>
+  Matrix<T, ORD1> operator+ (const Matrix<T, ORD1> & a, const Matrix<T, ORD2> & b)
+  {
+    Matrix<T, ORD1> sum(a.Rows(), a.Cols());
+    for (size_t i = 0; i < a.Rows(); i++){
+        for(size_t j = 0; j < a.Cols() - 1; j++){
+            sum(i, j) = a(i, j) + b(i, j);
+        }
+    }
+    return sum;
+  }
+
+}
+
+#endif
--- tests/test_matrix.cc
+++ tests/test_matrix.cc
@@ -1,8 +1,32 @@
 #include <iostream>
 
-#include <vector.h>
+#include <matrix.h>
 
 namespace bla = ASC_bla;
 
+int main()
+{
+    size_t m = 3;
+    size_t n = 5; 
+    bla::Matrix<double,bla::ColMajor> a(m, n), c(m, n);
+    bla::Matrix<double,bla::ColMajor> b(m, n);
 
-// here test for Matrix class
+    int k = 0;
+    for (size_t i = 0; i < a.Rows(); i++){
+        for (size_t j = 0; j < a.Cols(); j++){
+            a(i, j) = k;
+            b(i, j) = -k;
+            k++;
+        }
+    }
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    c = b;
+    c(1, 2) = 17;
+    std::cout << "c = " << c << std::endl;
+    c = a + b;
+    std::cout << "a + b = " << c << std::endl;
+    c = b.transpose();
+    std::cout << "b^T = " << c << std::endl;
+}

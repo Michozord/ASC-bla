15f6ed701cd456d3dd0982cf32a63ceed6c68d0d
Tobias Steinkellner <steinkellner.tobias1@gmail.com>
2023-10-30 15:19:40 +0100

    added primitive benchmark for C++ code

--- py_tests/benchmark.py
+++ py_tests/benchmark.py
@@ -1,6 +1,6 @@
 import timeit
 #primitve benchmark
-#to do: measure the C++ code separately, compare results to numpy
+#to do: implement buffer protocol, think how you could effectively measure the time for C++ code separately
 #runtime around 20 sec
 setup10='''
 import random
@@ -40,13 +40,13 @@ z1=fill(1000)
 z2=fill(1000)'''
 
 code10='''
-x1*x2
+x=x1*x2
 '''
 code100='''
-y1*y2
+y=y1*y2
 '''
 code1000='''
-z1*z2
+z=z1*z2
 '''
 time10=0
 for k in range(5):
@@ -61,3 +61,19 @@ for k in range(5):
 print("the time for n=10 is:", time10/5)
 print("the time for n=100 is:", time100/5)
 print("the time for n=1000 is:", time1000/5)
+
+npsetup1000='''
+import numpy as np
+z1=np.random.rand(1000,1000)*10
+z2=np.random.rand(1000,1000)*10
+'''
+
+npcode1000='''
+np.matmul(z1,z2)
+'''
+
+nptime1000=0
+for k in range(5):
+    nptime1000+=timeit.timeit(setup=npsetup1000,stmt=npcode1000,number=3)/3
+
+print("numpy time for n=1000 is:", nptime1000/5)

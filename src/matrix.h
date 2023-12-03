1360fafbb6258297e93b8de2813d4c2a7f753252
Michozord <66361088+Michozord@users.noreply.github.com>
2023-12-03 21:48:30 +0100

    Parallelisation in matrix-matrix multiplication

--- src/matrix.h
+++ src/matrix.h
@@ -8,6 +8,11 @@
 #include <utility>
 #include "../ASC-HCP/src/simd.h"
 #include "../ASC-HCP/src/simd_avx.h"
+#include <../ASC-HCP/src/taskmanager.cc>
+#include <../ASC-HCP/src/timer.cc>
+#include<functional>
+
+
 
 namespace ASC_bla
 {   //forward declaration of Vector class
@@ -250,12 +255,14 @@ namespace ASC_bla
   auto operator* (const Matrix<TA, ORD> & a, const Matrix<TB, ORD> & b)
   {
     assert(a.Width() == b.Height());
+    ASC_HPC::StartWorkers(3);
     const size_t SW = 4;
     Matrix<TA, ORD> c(a.Height(), b.Width());
-    size_t i;
-    for(i=0; i<a.Height()-3; i+=SW){ 
-      size_t j;
-      for(j=0; j<b.Width()-3; j+=SW){
+    ASC_HPC::RunParallel(a.Height()/SW, [&a, &b, &c, SW] (int i_step, int size)
+    { 
+      size_t i = i_step * SW;
+      ASC_HPC::RunParallel(b.Width()/SW, [&a, &b, &c, i, SW] (int j_step, int size){
+        size_t j = j_step * SW;
         ASC_HPC::SIMD<TA, SW> simd_sum0(0.);
         ASC_HPC::SIMD<TA, SW> simd_sum1(0.);
         ASC_HPC::SIMD<TA, SW> simd_sum2(0.);
@@ -270,36 +277,38 @@ namespace ASC_bla
           simd_sum2 += b(k, j+2) * simd_temp2;
           simd_sum3 += b(k, j+3) * simd_temp3;
         }
-        c(i,j) = simd_sum0[0];
-        c(i+1,j) = simd_sum0[1];
-        c(i+2,j) = simd_sum0[2];
-        c(i+3,j) = simd_sum0[3];
-        c(i,j+1) = simd_sum1[0];
-        c(i+1,j+1) = simd_sum1[1];
-        c(i+2,j+1) = simd_sum1[2];
-        c(i+3,j+1) = simd_sum1[3];
-        c(i,j+2) = simd_sum2[0];
-        c(i+1,j+2) = simd_sum2[1];
-        c(i+2,j+2) = simd_sum2[2];
-        c(i+3,j+2) = simd_sum2[3];
-        c(i,j+3) = simd_sum3[0];
-        c(i+1,j+3) = simd_sum3[1];
-        c(i+2,j+3) = simd_sum3[2];
-        c(i+3,j+3) = simd_sum3[3];
-      }
-      for (j; j<b.Width(); j++){
+        c(i,j) = simd_sum0.Val()[0];
+        c(i+1,j) = simd_sum0.Val()[1];
+        c(i+2,j) = simd_sum0.Val()[2];
+        c(i+3,j) = simd_sum0.Val()[3];
+        c(i,j+1) = simd_sum1.Val()[0];
+        c(i+1,j+1) = simd_sum1.Val()[1];
+        c(i+2,j+1) = simd_sum1.Val()[2];
+        c(i+3,j+1) = simd_sum1.Val()[3];
+        c(i,j+2) = simd_sum2.Val()[0];
+        c(i+1,j+2) = simd_sum2.Val()[1];
+        c(i+2,j+2) = simd_sum2.Val()[2];
+        c(i+3,j+2) = simd_sum2.Val()[3];
+        c(i,j+3) = simd_sum3.Val()[0];
+        c(i+1,j+3) = simd_sum3.Val()[1];
+        c(i+2,j+3) = simd_sum3.Val()[2];
+        c(i+3,j+3) = simd_sum3.Val()[3];
+      });
+      ASC_HPC::RunParallel(b.Width()%SW, [&a, &b, &c, i, SW] (int j_step, int size){
+        size_t j = b.Width() - (b.Width()%SW) + j_step;
         ASC_HPC::SIMD<TA, SW> simd_sum0(0.);
         for (size_t k=0; k<a.Width(); k++){
           ASC_HPC::SIMD<TA, SW> simd_temp0(a(i,k), a(i+1,k), a(i+2,k), a(i+3,k));
           simd_sum0 += b(k, j) * simd_temp0;
         }
-        c(i,j) = simd_sum0[0];
-        c(i+1,j) = simd_sum0[1];
-        c(i+2,j) = simd_sum0[2];
-        c(i+3,j) = simd_sum0[3];
-      }
-    }
-    for (i; i<a.Height(); i++){
+        c(i,j) = simd_sum0.Val()[0];
+        c(i+1,j) = simd_sum0.Val()[1];
+        c(i+2,j) = simd_sum0.Val()[2];
+        c(i+3,j) = simd_sum0.Val()[3];
+      });
+    });
+    ASC_HPC::RunParallel(a.Height()%SW, [&a, &b, &c, SW] (int i_step, int size){
+      size_t i = a.Height() - (a.Height()%SW) + i_step;
       size_t j = 0;
       for (j; j<b.Width()-3; j+=SW){
         ASC_HPC::SIMD<TA, SW> simd_sum0(0.);
@@ -307,10 +316,10 @@ namespace ASC_bla
           ASC_HPC::SIMD<TA, SW> simd_temp0(b(k, j), b(k, j+1), b(k, j+2), b(k, j+3));
           simd_sum0 += a(i, k) * simd_temp0;
         }
-        c(i,j) = simd_sum0[0];
-        c(i,j+1) = simd_sum0[1];
-        c(i,j+2) = simd_sum0[2];
-        c(i,j+3) = simd_sum0[3];
+        c(i,j) = simd_sum0.Val()[0];
+        c(i,j+1) = simd_sum0.Val()[1];
+        c(i,j+2) = simd_sum0.Val()[2];
+        c(i,j+3) = simd_sum0.Val()[3];
       }
       for (j; j<b.Width(); j++){
         TA sum = 0;
@@ -319,7 +328,8 @@ namespace ASC_bla
         }
         c(i,j)=sum;
       }
-    }
+    });
+    ASC_HPC::StopWorkers();
     return c;
   }
 
